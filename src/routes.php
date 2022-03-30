ff0432f1de6cb8677a7080fc963c4a32bc6c451e
Jewel Rana <jewelrana.dev@gmail.com>
2022-03-30 18:07:34 +0600

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -3,19 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Rajtika\Firebase\Services\Firebase;
 
-Route::get('firebase', function(){
-    return 'Congratulation!! Your firebase initialized.' . config('firebase_api_key');
-});
-Route::get('firebase/send', function(){
-    $token = request('token');
-    if($token) {
-        $response = Firebase::to($token)
-            ->setID(1)
-            ->setTitle('Hello test')
-            ->setBody('You just received my notification, right?')
-            ->send();
+// Route::get('firebase', function(){
+//     return 'Congratulation!! Your firebase initialized.' . config('firebase_api_key');
+// });
+// Route::get('firebase/send', function(){
+//     $token = request('token');
+//     if($token) {
+//         $response = Firebase::to($token)
+//             ->setID(1)
+//             ->setTitle('Hello test')
+//             ->setBody('You just received my notification, right?')
+//             ->send();
 
-        dd($response);
-    }
-    dd('Token not provided');
-});
+//         dd($response);
+//     }
+//     dd('Token not provided');
+// });
